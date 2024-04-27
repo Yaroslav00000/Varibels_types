951d4aa982da70440490d634af54c248fa2226ee
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 23:18:42 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    double R1,R2,R3;
+    cout << "Enter R1 = ";
+    cin >> R1;
+    cout << "Enter R2 = ";
+    cin >> R2;
+    cout << "Enter R3 = ";
+    cin >> R3;
+    double R = (1 / R1) + (1 / R2) + (1 / R3);
+    double R_O = 1 / R;
+    cout << "R0= " << R_O << endl;
     system("pause");
     return 0;
 }
