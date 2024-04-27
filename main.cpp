6f5676d2e2a4ec02331406f1f019dfab91ca6c86
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 23:25:45 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <Windows.h>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    double Length;
+    cout << "Length = ";
+    cin >> Length;
+    double radius = Length / (2 * 3.14);
+    double area = 3.14 * (radius* radius);
+    cout << "Area = " << area << endl;
     system("pause");
     return 0;
 }
