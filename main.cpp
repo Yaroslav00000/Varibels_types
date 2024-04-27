cb11122bf7719fff3629e14c726a3c521d9950cd
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 23:36:06 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <Windows.h>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    double speed,time,acceleration;
+    cout << "Speed = ";
+    cin >> speed;
+    cout << "time = ";
+    cin >> time;
+    cout << "acceleration = ";
+    cin >> acceleration;
+    double Distance = (speed * time) + ((acceleration * (time * time)/2));
+    cout << "Distance = " << Distance << " m" << endl;
     system("pause");
     return 0;
 }
