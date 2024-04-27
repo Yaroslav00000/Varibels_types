4a3fcbc062ecb8f38ce65bed99372ca706ca59cb
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 22:37:26 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -2,8 +2,44 @@
 #include <Windows.h>
 using namespace std;
 
-int main() {
+enum Color
+{
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta = 13,
+    LightBrown = 14,
 
+};
+int main() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    char rucka = 205;
+    cout << "\tName";
+    cout << "\t   :";
+    SetConsoleTextAttribute(hConsole, 6);
+    cout << "  \"The war and the peace\"" << endl;
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << "\tAvtor";
+    cout << "\t   :";
+    cout << "    L.N. Tolstoj" << endl;
+    cout << "\tIzdatelstvo";
+    cout << ":";
+    cout << "   Piter" << endl;
+    cout << "\tPages";
+    cout << "\t   :";
+    SetConsoleTextAttribute(hConsole, 13);
+    cout << "  500" << endl;
+    SetConsoleTextAttribute(hConsole, 7);
     system("pause");
     return 0;
 }
